a28262a28f85b0699dc41f39b2f374f90f707381
Jay Kudo <jay.kudo@avanade.com>
2021-01-26 09:22:21 -0800

    Updated to include Hooks and State

--- training-app/src/App.js
+++ training-app/src/App.js
@@ -1,57 +1,115 @@
+// // -- Name Tag Non-Hooks App --
+// import "./App.scss";
+// import NameTag from './components/nameTag';
+
+// const nameStyle = {
+//   color: "gray",
+//   border: "1px solid gray",
+//   paddingTop: "5px",
+//   paddingBottom: "5px",
+//   width: "300px",
+// };
+
+// const nameTagTitle = {
+//   color: "yellow",
+//   borderColor: "yellow",
+//   borderStyle: "dotted"
+// }
+
+// // -- Higher Order Components --
+// const makeGreen = BaseComponent => props => {
+//   const addGreen = {
+//     style: {
+//       color: "green"
+//     }
+//   }
+  
+//   const newProps = {
+//     ...props, ...addGreen
+//   }
+
+//   return <BaseComponent {...newProps} />
+// }
+
+// const removeInline = BaseComponent => props => {
+//   const newProps = {...props};
+//   delete newProps.style;
+//   return <BaseComponent></BaseComponent>
+// }
+
+// const GreenNameTag = makeGreen(NameTag);
+// const CleanNameTag = removeInline(NameTag);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* Spread operator for JSX attributes */}
+//         <h1 style={{...nameStyle,...nameTagTitle}}>Names List</h1>
+//         <GreenNameTag firstName="Peter" lastName="Smith"></GreenNameTag>
+//         <CleanNameTag firstName="John" lastName="Smith"></CleanNameTag>
+//         <NameTag firstName="Jill" lastName="Smith"></NameTag>
+//         <NameTag></NameTag>
+//       </header>
+//     </div>
+//   );
+// }
+
+// -- Name Tag Hooks App --
+import React, {useState} from 'react'
 import "./App.scss";
 import NameTag from './components/nameTag';
 
-const nameStyle = {
-  color: "gray",
-  border: "1px solid gray",
-  paddingTop: "5px",
-  paddingBottom: "5px",
-  width: "300px",
-};
-
-const nameTagTitle = {
-  color: "yellow",
-  borderColor: "yellow",
-  borderStyle: "dotted"
-}
-
-// -- Higher Order Components --
-const makeGreen = BaseComponent => props => {
-  const addGreen = {
-    style: {
-      color: "green"
-    }
-  }
-  
-  const newProps = {
-    ...props, ...addGreen
-  }
-
-  return <BaseComponent {...newProps} />
-}
-
-const removeInline = BaseComponent => props => {
-  const newProps = {...props};
-  delete newProps.style;
-  return <BaseComponent></BaseComponent>
-}
-
-const GreenNameTag = makeGreen(NameTag);
-const CleanNameTag = removeInline(NameTag);
+const initialNames = [
+  {firstName:"Peter", lastName:"Smith"},
+  {firstName:"John", lastName:"Smith"},
+  {firstName:"Jill", lastName:"Smith"},
+  {firstName:"Mary", lastName:"Smith"}
+]
 
 function App() {
+  const [names, setNames] = useState(initialNames);
   return (
     <div className="App">
       <header className="App-header">
-        {/* Spread operator for JSX attributes */}
-        <h1 style={{...nameStyle,...nameTagTitle}}>Names List</h1>
-        <GreenNameTag firstName="Peter" lastName="Smith"></GreenNameTag>
-        <CleanNameTag firstName="John" lastName="Smith"></CleanNameTag>
-        <NameTag firstName="Jill" lastName="Smith"></NameTag>
-        <NameTag></NameTag>
+        <h1 className="name, title">Names List</h1>
+        {
+          names.map( (v,i) => {
+            return <NameTag key={`${i}${v.firstName}${v.lastName}`} firstName={v.firstName} lastName={v.lastName}></NameTag>
+          })
+        }
+
+        {/* <NameTag firstName={names[0].firstName} lastName={names[0].lastName}></NameTag>
+        <NameTag firstName={names[1].firstName} lastName={names[1].lastName}></NameTag>
+        <NameTag firstName={names[2].firstName} lastName={names[2].lastName}></NameTag> */}
       </header>
     </div>
   );
 }
+
+// // -- Age Hook App -- 
+// import React, {useState} from 'react'
+// import "./App.scss";
+
+// function App() {
+//   const [age, setAge] = useState(21);
+//   const ageUpHandle = () => {
+//     setAge(age + 1);
+//   }
+//   const ageDownHandle = () => {
+//     setAge(age - 1);
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Use State Hook</h1>
+//         <h2>Age: {age}</h2>
+//         <button onClick={ageUpHandle}>Age Up</button>
+//         <button onClick={ageDownHandle}>Age Down</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
